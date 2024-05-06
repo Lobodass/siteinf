71f2a5bbdd2e16aca8a605cfebbef66d5b16655f
Lobodass <158287475+Lobodass@users.noreply.github.com>
2024-05-06 14:48:19 +0300

    giykg

---
+++ marcet.js
@@ -0,0 +1,8 @@
+ console.log('Перевірка підключення файлу скриптів marcet.js')
+ let itemsDiv = document.getElementById("items");
+ console.log(itemsDiv)
+ if (itemsDiv) {
+    console.log(itemsDiv)
+ } else {
+    console.log('Блок товарів не знайдено')
+ }
